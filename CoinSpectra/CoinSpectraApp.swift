ae88c695b3e28da97db6afe54d57ee9f79d9df39
Artem Lushchan <alushan1997@gmail.com>
2024-03-04 19:41:31 +0000

    Initial Commit

---
+++ CoinSpectra/CoinSpectraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinSpectraApp.swift
+//  CoinSpectra
+//
+//  Created by Artem Lushchan on 2024-03-04.
+//
+
+import SwiftUI
+
+@main
+struct CoinSpectraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
